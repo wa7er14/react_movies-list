617513e9cc9584f979c3884ba820379123138478
yura <wa7er14@gmail.com>
2020-06-23 20:45:20 +0300

    Updates

---
+++ precache-manifest.e3c4eac52368f8f0f0fae520006af111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc11d6f4f4b4fab7fe4d1bc7eabcf1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74cc4768fab5abcf4163",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "c298ec6c6bf497c841e0",
+    "url": "./static/css/main.995370ef.chunk.css"
+  },
+  {
+    "revision": "74cc4768fab5abcf4163",
+    "url": "./static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c298ec6c6bf497c841e0",
+    "url": "./static/js/main.1852dab0.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.e3c4eac52368f8f0f0fae520006af111.js"
 );
 
 self.addEventListener('message', (event) => {
